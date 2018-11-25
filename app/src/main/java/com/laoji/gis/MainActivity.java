709e4d356f54d4009c472fb9f9ef0b00fc406e2d
laoji <laolaolaoji@163.com>
2018-11-25 16:24:58 +0800

    添加高德地图依赖,key,定位服务

--- app/src/main/java/com/laoji/gis/MainActivity.java
+++ app/src/main/java/com/laoji/gis/MainActivity.java
@@ -1,8 +1,20 @@
 package com.laoji.gis;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,17 +24,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    //高德地图相关
     public MapView mMapView;
+    public AMap aMap;
+    public LocationSource aMapLocationSource;
+    public AMapLocationClient aMapLocationClient;
+    public AMapLocationClientOption aMapLocationClientOption;
+    public AMapLocationListener aMapLocationListener;
+    public LocationSource.OnLocationChangedListener aMapOnLocationChangedListener;
+    public AMapLocation aMapLocation;
+    public UiSettings aMapUiSettings;
+
+    //要申请的权限
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION};
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 版本判断。当手机系统大于 23 时，才有必要去判断权限是否获取
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 检查该权限是否已经获取
+            for(String permission:permissions){
+                int i = ContextCompat.checkSelfPermission(this, permission);
+                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 如果没有授予该权限，就去提示用户请求
+                    showDialogTipUserRequestPermission();
+                }
+            }
+
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,12 +91,92 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        findViewById(R.id.AMap_MapView);
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.AMap_MapView);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
+        aMap = mMapView.getMap();
+        aMapUiSettings = aMap.getUiSettings();
+        //隐藏logo
+        aMapUiSettings.setLogoBottomMargin(-50);
+        //隐藏缩放控件
+        aMapUiSettings.setZoomControlsEnabled(false);
+        AMapInit();
+    }
 
+    /**
+     * 高德地图初始化
+     */
+    public void AMapInit() {
+        //定位监听初始化
+        AMapLocationListenerInit();
+        //定位资源初始化
+        AMapLocationSourceInit();
+        // 设置定位监听
+        aMap.setLocationSource(aMapLocationSource);
+        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+
+
+    }
+
+    /**
+     * 初始化高德地图定位资源
+     */
+    public void AMapLocationSourceInit() {
+        aMapLocationSource = new LocationSource() {
+            @Override
+            public void activate(OnLocationChangedListener onLocationChangedListener) {
+                aMapOnLocationChangedListener = onLocationChangedListener;
+                if (aMapLocationClient == null) {
+                    //初始化定位
+                    aMapLocationClient = new AMapLocationClient(getApplicationContext());
+                    //初始化定位参数
+                    aMapLocationClientOption = new AMapLocationClientOption();
+                    //设置定位回调监听
+                    aMapLocationClient.setLocationListener(aMapLocationListener);
+                    //设置为高精度定位模式
+                    aMapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+                    //设置定位参数
+                    aMapLocationClient.setLocationOption(aMapLocationClientOption);
+                    // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+                    // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+                    // 在定位结束后，在合适的生命周期调用onDestroy()方法
+                    // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+                    aMapLocationClient.startLocation();//启动定位
+                }
+            }
+
+            @Override
+            public void deactivate() {
+                aMapOnLocationChangedListener = null;
+                if (aMapLocationClient != null) {
+                    aMapLocationClient.stopLocation();
+                    aMapLocationClient.onDestroy();
+                }
+                aMapLocationClient = null;
+            }
+        };
+    }
+
+    /**
+     * 初始化高德地图定位监听
+     */
+    public void AMapLocationListenerInit() {
+        aMapLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation Location) {
+                if (aMapOnLocationChangedListener != null && Location != null) {
+                    if (Location != null && Location.getErrorCode() == 0) {
+                        aMapLocation=Location;
+                        aMapLocationListener.onLocationChanged(Location);// 显示系统小蓝点
+                    } else {
+                        String errText = "定位失败," + Location.getErrorCode() + ": " + Location.getErrorInfo();
+                        Log.e("AmapErr", errText);
+                    }
+                }
+            }
+        };
     }
 
     @Override
@@ -114,6 +241,9 @@ public class MainActivity extends AppCompatActivity
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
+        if (null != aMapLocationClient) {
+            aMapLocationClient.onDestroy();
+        }
     }
 
     @Override
@@ -136,4 +266,102 @@ public class MainActivity extends AppCompatActivity
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
     }
+    // 提示用户该请求权限的弹出框
+    private void showDialogTipUserRequestPermission() {
+        new AlertDialog.Builder(this)
+                .setTitle("权限不可用")
+                .setMessage("由于定位需要获取一些本机权限；\n否则，您将无法正常使用")
+                .setPositiveButton("立即开启", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startRequestPermission();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+    // 开始提交请求权限
+    private void startRequestPermission() {
+        ActivityCompat.requestPermissions(this, permissions, 321);
+    }
+    // 用户权限 申请 的回调方法
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 321) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    // 判断用户是否 点击了不再提醒。(检测该权限是否还可以申请)
+                    boolean b = shouldShowRequestPermissionRationale(permissions[0]);
+                    if (!b) {
+                        // 用户还是想用我的 APP 的
+                        // 提示用户去应用设置界面手动开启权限
+                        showDialogTipUserGoToAppSettting();
+                    } else
+                        finish();
+                } else {
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // 提示用户去应用设置界面手动开启权限
+    private void showDialogTipUserGoToAppSettting() {
+        dialog = new AlertDialog.Builder(this)
+                .setTitle("权限不可用")
+                .setMessage("请在-应用设置-权限-中，允许支付宝使用存储权限来保存用户数据")
+                .setPositiveButton("立即开启", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 跳转到应用设置界面
+                        goToAppSetting();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    // 跳转到当前应用的设置界面
+    private void goToAppSetting() {
+        Intent intent = new Intent();
+
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+
+        startActivityForResult(intent, 123);
+    }
+
+    //
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 检查该权限是否已经获取
+                int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 提示用户应该去应用设置界面手动开启权限
+                    showDialogTipUserGoToAppSettting();
+                } else {
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.dismiss();
+                    }
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 }
