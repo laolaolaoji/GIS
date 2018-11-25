f70289cd88f5ff5a5e65665faa543381465541a1
laoji <laolaolaoji@163.com>
2018-11-25 19:26:31 +0800

    设置初始显示缩放级和位置

--- app/src/main/java/com/laoji/gis/MainActivity.java
+++ app/src/main/java/com/laoji/gis/MainActivity.java
@@ -28,6 +28,8 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.CameraUpdate;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
@@ -131,7 +133,8 @@ public class MainActivity extends AppCompatActivity
             aMapLocationClient.setLocationListener(aMapLocationListener);
             aMapLocationClient.startLocation();
         }
-
+        LatLng latLng=new LatLng(30.621716,113.542914);
+        aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,3));
         aMapLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         aMapLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMapLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
